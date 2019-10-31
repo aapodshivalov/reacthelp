ed6a8ff688a6ea4db4c012682e73a72ba980b6ac
Aleksandr0012 <32939018+Aleksandr0012@users.noreply.github.com>
2019-10-31 22:29:31 +0400

    test

---
+++ src/help/info_1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Info_1 extends Component {
+  render() {
+    return (
+      <div>
+        text
+      </div>
+    );
+  }
+}
+
+export default Info_1;
